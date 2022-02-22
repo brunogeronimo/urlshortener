37e97656b1a4a2039036fba3671dcce936c472a1
Bruno Geronimo <hi@bruno.works>
2022-02-22 19:18:03 +0100

    Adds isFallbackPermanentRedirect into parameters

--- types.go
+++ types.go
@@ -1,11 +1,12 @@
 package main
 
 type ShortUrl struct {
-	Url string `json:"url"`
+	Url         string `json:"url"`
 	Destination string `json:"destination"`
 }
 
 type ConfigurationFile struct {
-	FallbackUrl string `json:"fallbackUrl"`
-	Urls []ShortUrl `json:"urls"`
+	FallbackUrl                 string     `json:"fallbackUrl"`
+	IsFallbackPermanentRedirect bool       `json:"isFallbackPermanentRedirect"`
+	Urls                        []ShortUrl `json:"urls"`
 }
